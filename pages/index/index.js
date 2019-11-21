e0c243558fb76debf4355c864f7f3ad657f6e469
yougexiaoluo <1019134495@qq.com>
2019-11-22 00:16:26 +0800

    首页导航展示

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,17 +1,26 @@
-import { request } from '../../request/index.js'
+import {
+  request
+} from '../../request/index.js'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    swiperData: []
+    swiperData: [],
+    cateList: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    this.getSwiperData()
+    this.getCateList()
+  },
+
+  // 获取轮播图数据
+  getSwiperData: function () {
     request({
       url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata'
     }).then(res => {
@@ -21,52 +30,63 @@ Page({
     })
   },
 
+  // 获取导航数据
+  getCateList: function () {
+    request({
+      url: 'https://api.zbztb.cn/api/public/v1/home/catitems'
+    }).then(res => {
+      this.setData({
+        cateList: res.data.message
+      })
+    })
+  },
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
   }
 })
