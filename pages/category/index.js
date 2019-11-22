6bd602297dd18b08b266dd7d76925f9925c48955
mr_luo <mr_luo2016@163.com>
2019-11-22 16:12:29 +0800

    实现分类静态页面展示，以及简单的商品类型数据切换展示

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,19 +1,45 @@
-// pages/category/index.js
+import { request } from '../../request/index.js'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    leftMenuList: [], // 左侧菜单数据
+    rightContentList: [], // 右侧菜单数据
+    currentIndex: 0, // 当前页码下标
   },
-
+  // 接收返回的数据
+  cates: [],
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getCates()
   },
+
+  // 获取分类数据
+  getCates() {
+    request({
+      url: 'https://api.zbztb.cn/api/public/v1/categories'
+    }).then(res => {
+      this.cates = res.data.message
+      let leftMenuList = this.cates.map(item => item.cat_name)
+      let rightContentList = this.cates[0].children
+      this.setData({
+        leftMenuList,
+        rightContentList
+      })
+    })
+  },
+
+  // 切换菜单内容
+  changeMenuHandle(e) {
+    let currentIndex = e.currentTarget.dataset.index
+    let rightContentList = this.cates[currentIndex].children
+    this.setData({ currentIndex, rightContentList })
+  },
+
 
   /**
    * 生命周期函数--监听页面初次渲染完成
