1d45070a4e5acda499583656c30dafa580148a86
mr_luo <mr_luo2016@163.com>
2019-12-04 17:35:35 +0800

    代码进行优化，添加显示商品数量，以及没有商品预览时的显示效果

--- cloudfunctions/product/index.js
+++ cloudfunctions/product/index.js
@@ -1,14 +1,35 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
-
-cloud.init()
-
+cloud.init({ env: 'cloud-app-nbjb7' })
+const db = cloud.database()
+const lists = db.collection('lists')
 // 云函数入口函数
 exports.main = async (event, context) => {
-  const wxContext = cloud.getWXContext()
-  return {
-    event,
-    openid: wxContext.OPENID,
-    appid: wxContext.APPID,
+  /**
+   * 加入购物车功能
+   * 1. 查找数据库中是否存在此商品
+   *    a. 根据商品ID进行查找
+   * 2. 如果存在，那么该商品数量 +1
+   * 3. 不存在，那么该商品数量默认为 1
+   */
+  console.log(event, context)
+  try {
+    let { product } = event
+    let { OPENID, APPID } = cloud.getWXContext()
+    let { data } = await lists.get()
+    console.log('data == ', data)
+    return {
+      msg: '添加商品成功',
+      success: true,
+      code: 1,
+      OPENID,
+      APPID
+    }
+  } catch (e) {
+    return {
+      msg: '添加商品失败',
+      success: false,
+      code: -1
+    }
   }
 }
--- miniprogram/pages/goods_detail/index.js
+++ miniprogram/pages/goods_detail/index.js
@@ -1,128 +1,66 @@
 import { request } from "../../request/index.js"
-
-// pages/goods_detail/index.js
+const db = wx.cloud.database()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    goodsId: '',
-    pics: [], // 轮播图数据
-    goodsName: '', // 商品名
-    goodsIntroduce: '', // 商品描述
-    goodsPrice: '', // 商品价格
-    previewUrls: [], // 商品预览url
+    goodsObj: {}, // 显示的信息
   },
+  goodsInfo: {}, // 商品对象
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
     let { goodsId } = options
-    this.setData({ goodsId })
-    this.getGoodsDetail()
-    // 调用云函数
-    // wx.cloud.callFunction({
-    //   name: 'product',
-    //   data: {
-    //     a: 1,
-    //     b: 2
-    //   }
-    // }).then(res => {
-    //   console.log(res)
-    // }).catch(err => {
-    //   console.log(err)
-    // })
+    this.getGoodsDetail(goodsId)
+    // this.addProduct()
+  },
+
+  // 加入购物车
+  addProduct () {
+    wx.cloud.callFunction ({
+      name: 'product',
+      data: {
+        product: {}
+      }
+    }).then(res => {
+      console.log('res == ', res)
+    }).catch(err => {
+      console.log('err == ', err)
+    })
   },
 
   // 商品全屏预览
   previewImage (e) {
     let { url } = e.currentTarget.dataset
-    let { previewUrls } = this.data
+    let urls = this.goodsInfo.pics.map(item => item.pics_mid_url)
     wx.previewImage({
       current: url,
-      urls: previewUrls
+      urls
     })
   },
 
-  // 处理商品全屏预览url
-  handlePreviewUrls () {
-    let { pics, previewUrls } = this.data
-    pics.map(item => previewUrls.push(item.pics_mid_url))
-    this.setData({ previewUrls })
-  },
-
   // 获取商品详情
-  async getGoodsDetail () {
-    let { goodsId } = this.data
+  async getGoodsDetail (goodsId) {
     let res = await request({
       url: '/goods/detail',
       data: { goods_id: goodsId }
     })
-    let {
-      pics,
-      goods_name,
-      goods_introduce,
-      goods_price
-    } = res
+    this.goodsInfo = res
+    // console.log('res === ', res)
     // 设置data
     this.setData({
-      pics,
-      goodsName: goods_name,
-      // 这种做法不正确，只是暂时的解决方案，最终还是需要跟后台开发人员进行协商 --> 改为其他格式的图片
-      goodsIntroduce: goods_introduce.replace(/\.webp/g, '.jpg'),
-      goodsPrice: goods_price
+      goodsObj: {
+        pics: res.pics,
+        goodsName: res.goods_name,
+        // 这种做法不正确，只是暂时的解决方案，最终还是需要跟后台开发人员进行协商 --> 改为其他格式的图片
+        goodsIntroduce: res.goods_introduce.replace(/\.webp/g, '.jpg'),
+        goodsPrice: res.goods_price,
+        goodsNumber: res.goods_number,
+      }
     })
-    this.handlePreviewUrls()
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
   }
 })
